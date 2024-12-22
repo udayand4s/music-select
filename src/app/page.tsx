c8387f747067ef7626a1f0089615c4b41aa87661
Coochiee <sugarytchannel@gmail.com>
2024-12-22 21:56:10 +0530

    provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Appbar } from './components/Appbar'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div>
+      <Appbar/>
+
+      </div>
   )
 }
 
